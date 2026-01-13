aa311be9d8ebd6ca7fe48e0ef00f86f1cdb66239
jjerry-o0o <alfndp25@gmail.com>
2026-01-13 14:57:37 +0900

    eslint v8, prettier 설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import './index.css'
+import '@/index.css';
 
 function App() {
-    return (
-        <div className="flex items-center justify-center w-40 h-10 bg-amber-200">
-            <p className="text-pink-500">Vite + React</p>
-        </div>
-    )
+  return (
+    <div className="flex items-center justify-center w-40 h-10 bg-amber-200">
+      <p className="text-pink-500">Vite + React </p>
+    </div>
+  );
 }
 
-export default App
+export default App;
