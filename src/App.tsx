0144ee10b8f70252e8ea93e8db1b8889e63b3e94
jjerry-o0o <alfndp25@gmail.com>
2026-01-14 09:10:45 +0900

    shadcn button 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import '@/index.css';
+import { Button } from '@/components/ui/button';
 
 function App() {
   return (
-    <div className="flex items-center justify-center w-40 h-10 bg-amber-200">
+    <div className="flex flex-col items-center justify-center gap-4 w-full h-50 bg-amber-200">
       <p className="text-pink-500">Vite + React </p>
+      <Button>Default</Button>
+      <Button variant="outline">Outline</Button>
     </div>
   );
 }
